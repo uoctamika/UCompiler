c34811991f4ef280a46ae35b91031a694db1543d
Uoc Azizah <uocdev.github.io@gmail.com>
2026-03-07 19:35:09 +0800

    Remove warning message. Unused

--- include/ucc/tools/pch.h
+++ include/ucc/tools/pch.h
@@ -1,10 +1,6 @@
 #ifndef PCH_H
 #define PCH_H
 
-#ifndef PCH_COMPILED
-    #warning "this is Precompiled Header, run 'make pch BUILD=pch' before build this project sources trees"
-#endif
-
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
