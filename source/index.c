2e647637b2ed44d9d86c538c3eb088354836f600
Uoc Azizah <uocdev.github.io@gmail.com>
2026-03-07 21:02:19 +0800

    Add more spaces for header comments

--- source/index.c
+++ source/index.c
@@ -1,15 +1,15 @@
 /*
-* File       : index.c
-* classic ID : UCC-0091-4426
-* UUID       : 4383df26-c39a-4461-8be7-71a4bd51f10b
+* File         : index.c
+* classic ID   : UCC-0091-4426
+* UUID         : 4383df26-c39a-4461-8be7-71a4bd51f10b
 *
-* Type       : Entry Point
-* Description: Main entry point for this program
+* Type         : Entry Point
+* Description  : Main entry point for this program
 *
-* Author     : UocDev
-* Author ID  : 1
+* Author       : UocDev
+* Author ID    : 1
 *
-* License    : GNU GPL V2
+* License      : GNU GPL V2
 *
 */
 
