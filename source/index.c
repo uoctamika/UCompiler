b732f960f280638913f7d1db1cfa1dec692ed0f2
Uoc Azizah <uocdev.github.io@gmail.com>
2026-03-07 20:41:45 +0800

    Create metadata for all files C

--- source/index.c
+++ source/index.c
@@ -1,8 +1,23 @@
-#include <ucc/tools/pch.h>
+/*
+* File       : index.c
+* classic ID : UCC-0091-4426
+* UUID       : 4383df26-c39a-4461-8be7-71a4bd51f10b
+*
+* Type       : Entry Point
+* Description: Main entry point for this program
+*
+* Author     : UocDev
+* Author ID  : 1
+*
+* License    : GNU GPL V2
+*
+*/
 
+#include <ucc/tools/pch.h>
 
 int main(void)
 {
+
 
   printf("Hello World\n");
 
