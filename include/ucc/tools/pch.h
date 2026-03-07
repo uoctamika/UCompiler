43047c0e5087deb9ab5ac3ba2486099c0dbcb7db
Uoc Azizah <uocdev.github.io@gmail.com>
2026-03-08 06:46:50 +0800

    add metadata to pch.h

--- include/ucc/tools/pch.h
+++ include/ucc/tools/pch.h
@@ -1,3 +1,18 @@
+/*
+* File         : pch.h
+* classic ID   : UCC-8270-0018
+* UUID         : 736d0c95-dd19-4cdb-b62b-1d0bd1553ffb
+*
+* Type         : Precompiled Header
+* Description  : Precompiled Header for fast compilation times
+*
+* Author       : UocDev
+* Author ID    : 1
+*
+* License      : GNU GPL V2
+*
+*/
+
 #ifndef PCH_H
 #define PCH_H
 
