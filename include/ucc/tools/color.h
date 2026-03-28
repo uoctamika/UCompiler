23f5f3518580911ef79c29ffb2ca38b88d7dbd5d
Uoc Azizah <uocdev.github.io@gmail.com>
2026-03-28 16:09:13 +0800

    Add new colors scheme

--- include/ucc/tools/color.h
+++ include/ucc/tools/color.h
@@ -6,8 +6,21 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#define ANSI_BOLD_RED  "\033[1;31m"
-#define ANSI_RESET     "\033[0m"
+
+#define ANSI_RESET      "\033[0m"
+
+#define ANSI_BOLD_RED   "\033[1;31m"
+#define ANSI_BOLD_BLACK 
+
+
+#define ANSI_BLACK      "\x1b[30m"
+#define ANSI_RED        "\x1b[31m"
+#define ANSI_GREEN      "\x1b[32m"
+#define ANSI_YELLOW     "\x1b[33m"
+#define ANSI_BLUE       "\x1b[34m"
+#define ANSI_MAGENTA    "\x1b[35m"
+#define ANSI_CYAN       "\x1b[36m"
+#define ANSI_WHITE      "\x1b[37m"
 
 #endif
 
